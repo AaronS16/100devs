cbee00e1c5ca8f819fc39ad6ddd4e7e71b2c5546
Aaron <soteloaaron16@gmail.com>
2022-07-09 12:21:33 -0500

    done

--- javascript/aaron/Funtions Tasks/functions.js
+++ javascript/aaron/Funtions Tasks/functions.js
@@ -1,1 +1,35 @@
- 
+ /* The function will still work even without the else. It will continue on if the first statement is false */
+
+ function checkAge(age) {
+    return (age > 18) ? true : confirm('Did parents allow you?');
+  }
+  function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+  }
+
+  function min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+
+  function pow(x, n) {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  let x = prompt("x?", '');
+  let n = prompt("n?", '');
+  
+  if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer`);
+  } else {
+    alert( pow(x, n) );
+  }
