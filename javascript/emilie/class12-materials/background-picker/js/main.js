0dfb8499533b222e7cbb4c4c9555ab572fe96e8e
EmilieFlores <codingtime0000@gmail.com>
2022-07-09 06:35:11 -0500

    update

--- javascript/emilie/class12-materials/background-picker/js/main.js
+++ javascript/emilie/class12-materials/background-picker/js/main.js
@@ -1,19 +1,29 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('yellow').onclick = partyYellow
+document.getElementById('red').onclick = partyRed
 
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.backgroundColor = 'rgb(41, 1, 43)'
   document.querySelector('body').style.color = 'white'
 }
 
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.backgroundColor = 'rgb(11, 73, 31)'
   document.querySelector('body').style.color = 'white'
 }
 
 function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.backgroundColor = 'rgb(38, 84, 201)'
+  document.querySelector('body').style.color = 'white'
+}
+function partyYellow() {
+  document.querySelector('body').style.backgroundColor = 'rgb(241, 241, 151)'
+  document.querySelector('body').style.color = 'white'
+}
+function partyRed() {
+  document.querySelector('body').style.backgroundColor = 'rgb(177, 82, 82)'
   document.querySelector('body').style.color = 'white'
 }
