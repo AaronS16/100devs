1665dd99825016f17f39ae6f710f1c7d70901b89
Aaron <soteloaaron16@gmail.com>
2022-07-09 11:50:55 -0500

    teaching emilie

--- javascript/aaron/Variables_Tasks/tasks.js
+++ javascript/aaron/Variables_Tasks/tasks.js
@@ -12,4 +12,4 @@ let ourPlanet = 'Earth';
 
 let currentUser; 
 
-/* Birthday should be capatilized but age should not */
+/* Birthday should be capatilized but age should not. Birthday is known before hand but age is a value that can change */
--- javascript/aaron/class12-materials/class-weekend-boring/js/main.js
+++ javascript/aaron/class12-materials/class-weekend-boring/js/main.js
@@ -6,14 +6,14 @@ function check() {
   //Conditionals go here
 
 
-  if (day.toLowerCase() == "saturday" || day.toLowerCase() == "sunday")
+  if (day.toLowerCase() === "saturday" || day.toLowerCase() === "sunday") {
     document.querySelector('#placeToSee').innerHTML = "Weekend!"
-
-    else if (day.toLowerCase() == "monday" || day.toLowerCase() == "wednesday" || day.toLowerCase() == "Friday")
+    }
+    else if (day.toLowerCase() === "monday" || day.toLowerCase() === "wednesday" || day.toLowerCase() === "Friday") {
     document.querySelector('#placeToSee').innerHTML = "BORING!"
-
-    else (day.toLowerCase() == 'tuesday' || day.toLowerCase() == "thursday")
+    }
+    else {(day.toLowerCase() === "tuesday" || day.toLowerCase() === "thursday")
     document.querySelector('#placeToSee').innerHTML = "Class day"
-
+    }
 }
     
