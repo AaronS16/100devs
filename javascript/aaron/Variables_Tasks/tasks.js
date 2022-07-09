aa1d36f96b0e9f0e0211e5d91fa7195f1a821997
Aaron <soteloaaron16@gmail.com>
2022-07-09 10:46:31 -0500

    tasks

---
+++ javascript/aaron/Variables_Tasks/tasks.js
@@ -0,0 +1,15 @@
+let admin;
+let name;
+
+name = 'John';
+
+admin = name;
+
+alert(admin);
+
+
+let ourPlanet = 'Earth';
+
+let currentUser; 
+
+/* Birthday should be capatilized but age should not */
