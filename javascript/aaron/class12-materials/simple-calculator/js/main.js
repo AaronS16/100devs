22d62fdedb9e9594ca6741cfa67ea4958f46c8e9
Aaron <soteloaaron16@gmail.com>
2022-07-10 06:54:41 -0500

    Jave from scratch

--- javascript/aaron/class12-materials/simple-calculator/js/main.js
+++ javascript/aaron/class12-materials/simple-calculator/js/main.js
@@ -1,7 +1,7 @@
-let total = 0
+total = 0
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
+document.querySelector('#pumpkin').addEventListener('click', clearAll)
+document.querySelector('#dominosPizza').addEventListener('click', add3)
 document.querySelector('#zebra').addEventListener('click', add9)
 document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
 document.querySelector('.wings').addEventListener('click', add5)
@@ -9,38 +9,36 @@ document.querySelector('.burgers').addEventListener('click', sub6)
 document.querySelector('.ice').addEventListener('click', sub15)
 document.querySelector('.jomboy').addEventListener('click', add8)
 
-function makeZero() {
-  total = 0
-  document.querySelector('#placeToPutResult').innerText = total
-}
 
-function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
+function clearAll () {
+    total = 0
+    document.querySelector('#placeToPutResult').innerText = total
 }
-
-function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
+function add3 () {
+    total = total + 3
+    document.querySelector('#placeToPutResult').innerText = total
 }
-
-function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+function add9 () {
+    total = total + 9
+    document.querySelector('#placeToPutResult').innerText = total
+}
+function sub2 () {
+    total = total - 2
+    document.querySelector('#placeToPutResult').innerText = total
 }
 function add5 () {
-  total = total + 5
-  document.querySelector('#placeToPutResult').innerHTML = total
+    total = total + 5
+    document.querySelector('#placeToPutResult').innerText = total
 }
 function sub6 () {
-  total = total - 6
-  document.querySelector('#placeToPutResult').innerHTML = total
+    total = total - 6
+    document.querySelector('#placeToPutResult').innerText = total
 }
 function sub15 () {
-  total = total - 15
-  document.querySelector('#placeToPutResult').innerHTML = total
+    total = total - 15
+    document.querySelector('#placeToPutResult').innerText = total
 }
 function add8 () {
-  total = total + 8
-  document.querySelector('#placeToPutResult').innerHTML = total
+    total = total + 8
+    document.querySelector('#placeToPutResult').innerText = total
 }
