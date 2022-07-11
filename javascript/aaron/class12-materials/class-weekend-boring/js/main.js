0d6301abc9e0776793efe0dc74e00a68839882c7
Aaron <soteloaaron16@gmail.com>
2022-07-11 18:14:14 -0500

    Class

--- javascript/aaron/class12-materials/class-weekend-boring/js/main.js
+++ javascript/aaron/class12-materials/class-weekend-boring/js/main.js
@@ -1,19 +1,15 @@
 document.querySelector('#check').addEventListener('click', check)
-    
 
 function check () {
     const day = document.querySelector('#day').value
 
-if (day.toLowerCase() === 'saturday' || day.toLowerCase() === 'sunday') {
-    document.querySelector('#placeToSee').innerText = 'Weekend Yay!'
+if (day.toLowerCase() === 'sunday' || day.toLowerCase() === 'saturday'){
+    document.querySelector('#placeToSee').innerText = "Weekend YAY!!!"
     }
-    else if (day.toLowerCase() === 'monday' || day.toLowerCase() === 'wednesday' || day.toLowerCase() === 'friday') {
-    document.querySelector('#placeToSee').innerHTML = 'Boring Day'
+    else if (day.toLowerCase() === 'monday' || day.toLowerCase() === 'wednesday' || day.toLowerCase() === 'friday'){
+        document.querySelector('#placeToSee').innerText = 'Boring Days...'
     }
     else if (day.toLowerCase() === 'tuesday' || day.toLowerCase() === 'thursday') {
-    document.querySelector('#placeToSee').innerHTML = 'Class Day'
+        document.querySelector('#placeToSee').innerText = 'Learning Today'
     }
-    else {alert("Please enter in a day")
-    }
-   
 }
