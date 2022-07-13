766fef1900408fa69b1f1e1db9b188a1227433a3
Aaron <soteloaaron16@gmail.com>
2022-07-13 18:00:11 -0500

    picker

---
+++ javascript/aaron/class12-materials/background-picker/js/main..old.js
@@ -0,0 +1,21 @@
+document.querySelector('#purple').addEventListener('click', purpleParty)
+document.querySelector('#green').addEventListener('click', greenParty)
+document.querySelector('#blue').addEventListener('click', blueParty)
+document.querySelector('#red').addEventListener('click', redParty)
+
+function purpleParty () {
+    document.querySelector('body').style.backgroundColor = "rgba(155, 3, 160, 1)"
+    document.querySelector('body').style.color = "white"
+}
+function greenParty () {
+    document.querySelector('body').style.backgroundColor = "rgba(1, 102, 33, 1)"
+    document.querySelector('body').style.color = "white"
+}
+function blueParty () {
+    document.querySelector('body').style.backgroundColor = "rgba(12, 0, 182, 1)"
+    document.querySelector('body').style.color = "white"
+}
+function redParty () {
+    document.querySelector('body').style.backgroundColor = "rgba(255, 0, 0, 1)"
+    document.querySelector('body').style.color = 'white'
+}
