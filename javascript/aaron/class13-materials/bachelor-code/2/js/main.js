be18dc6159c156ddb13a1c93b45adb97f8e21772
Aaron <soteloaaron16@gmail.com>
2022-07-13 18:52:49 -0500

    bachelor 2

--- javascript/aaron/class13-materials/bachelor-code/2/js/main.js
+++ javascript/aaron/class13-materials/bachelor-code/2/js/main.js
@@ -6,20 +6,18 @@ document.querySelector('#andiNext').addEventListener('click', andiNext)
 document.querySelector('#claireNext').addEventListener('click', claireNext)
 document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
 
-function andiNext(){
-	claire.classList.add('hidden')
-	sharleen.classList.add('hidden')
-	andi.classList.toggle('hidden')
+function andiNext () {
+    claire.classList.add('hidden')
+    sharleen.classList.add('hidden')
+    andi.classList.toggle('hidden')
 }
-
-function claireNext(){
-	andi.classList.add('hidden')
-	sharleen.classList.add('hidden')
-	claire.classList.toggle('hidden')
+function claireNext () {
+    andi.classList.add('hidden')
+    sharleen.classList.add('hidden')
+    claire.classList.toggle('hidden')
 }
-
-function sharleenNext(){
-	andi.classList.add('hidden')
-	claire.classList.add('hidden')
-	sharleen.classList.toggle('hidden')
+function sharleenNext () {
+    andi.classList.add('hidden')
+    claire.classList.add('hidden')
+    sharleen.classList.toggle('hidden')
 }
