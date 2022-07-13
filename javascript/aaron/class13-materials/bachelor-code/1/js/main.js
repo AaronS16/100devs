f112d01d4eaea49b845d9f7bb52780c4aed75d1a
Aaron <soteloaaron16@gmail.com>
2022-07-13 06:21:21 -0500

    Bachelor

--- javascript/aaron/class13-materials/bachelor-code/1/js/main.js
+++ javascript/aaron/class13-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,6 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#finalRose').addEventListener('click', hide/*INSERTCODE*/)
 
 function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+	document.querySelector('#claire'/*INSERTCODE*/).style.display = 'none'
+	document.querySelector('#sharleen'/*INSERTCODE*/).style.display = 'none'
 }
