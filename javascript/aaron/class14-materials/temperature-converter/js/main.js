000324b4185e8be19e7e5fa37f2aae1aee204d33
Aaron <soteloaaron16@gmail.com>
2022-07-16 13:42:59 -0500

    temp

--- javascript/aaron/class14-materials/temperature-converter/js/main.js
+++ javascript/aaron/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,26 @@
 //Write your pseduo code first! 
+
+//create an input box to receive the temperature they would like to convert to
+
+
+//create a button to click once you enter in the temperature
+
+
+//have an empty h2 to place answer later
+
+
+
+//create a query to listen to click no the button
+
+
+
+//take the value from the input and run it through the function
+
+
+
+//make function convert celcius to fahrenheight
+
+
+
+//return the converted answer to the user
+
