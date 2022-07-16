36800dc95ebe12c07cf3311c57294a0960f9e21f
Aaron <soteloaaron16@gmail.com>
2022-07-16 14:28:53 -0500

    simple

--- javascript/aaron/class14-materials/simple-functions/js/main.js
+++ javascript/aaron/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,22 @@
 //create a function that adds two numbers and alerts the sum
 
+function add (x, y) {
+    alert(x + y)
+}
+
+add(7, 5)
+
 //create a function that multiplys three numbers and console logs the product
 
+function multiply (x, y, z) {
+    console.log(x * y * z)
+}
+multiply(5,8,2)
+
 //create a function that divides two numbers and returns the ???
+
+function divide (x, y) {
+    return x % y
+}
+
+console.log(divide(8, 3))
