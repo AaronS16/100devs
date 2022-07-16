0431c672cda07c7f10fc06f6efd8eb9834b9d0e2
EmilieFlores <codingtime0000@gmail.com>
2022-07-16 12:45:15 -0500

    update

--- javascript/emilie/class12-materials/background-picker/js/main.js
+++ javascript/emilie/class12-materials/background-picker/js/main.js
@@ -1,27 +1,30 @@
-document.getElementById('purple').onclick= partyPurple 
-document.getElementById('green').onclick= partyGreen 
-document.getElementById('blue').onclick= partyBlue 
-document.getElementById('yellow').onclick= partyYellow 
-document.getElementById('red').onclick= partyRed
+document.querySelector("#purple").addEventListener("click" , colorPurple)
+document.querySelector("#green").addEventListener("click" , colorGreen)
+document.querySelector("#blue").addEventListener("click" , colorBlue)
+document.querySelector("#yellow").addEventListener("click" , colorYellow)
+document.querySelector("#red").addEventListener("click" , colorRed)
 
-function partyPurple() {
-    document.querySelector('body').style.backgroundColor = 'rgb(41, 1, 43)'
-    document.querySelector('body').style.Color = 'white'
+function colorPurple(){
+document.querySelector('body').style.backgroundColor ="rgb(41, 1, 43)"
+document.querySelector('body').style.color = "white"
 }
 
-function partyGreen () {
-    document.querySelector('body').style.backgroundColor = 'rgb(11, 73, 31)'
-    document.querySelector('body').style.Color = 'white'
-}
-function partyBlue() {
-    document.querySelector('body').style.backgroundColor = 'rgb(38, 84, 201)'
-    document.querySelector('body').style.Color = 'white'
-}
-function partyYellow () {
-    document.querySelector('body').style.backgroundColor = 'rgb(241, 241, 151)'
-    document.querySelector('body').style.Color = 'white'
-}
-function partyRed() {
-    document.querySelector('body').style.backgroundColor = 'rgb(177, 82, 82)'
-    document.querySelector('body').style.Color = 'white'
-}
+function colorGreen(){
+    document.querySelector('body').style.backgroundColor ="rgb(11, 73, 31)"
+    document.querySelector('body').style.color = "white"
+    }
+    
+function colorBlue(){
+        document.querySelector('body').style.backgroundColor ="rgb(38, 84, 201)"
+        document.querySelector('body').style.color = "white"
+        }
+
+function colorYellow(){
+            document.querySelector('body').style.backgroundColor ="rgb(241, 241, 151)"
+            document.querySelector('body').style.color = "white"
+            }
+
+function colorRed(){
+                document.querySelector('body').style.backgroundColor ="rgb(177, 82, 82)"
+                document.querySelector('body').style.color = "white"
+                }
