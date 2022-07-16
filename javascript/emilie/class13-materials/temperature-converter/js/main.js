241c0946a33c646f05cc4e4bf0419232b693da14
EmilieFlores <codingtime0000@gmail.com>
2022-07-16 14:25:36 -0500

    update

--- javascript/emilie/class13-materials/temperature-converter/js/main.js
+++ javascript/emilie/class13-materials/temperature-converter/js/main.js
@@ -1,2 +1,9 @@
 //Write your pseduo code first! 
-document
+document.querySelector("#submit").addEventListener('click', numberConvert)
+
+function numberConvert() {
+    let infoNumb = document.querySelector('#celciusText').value
+    let temp = infoNumb * 9/5 + 32
+
+  document.querySelector("#placeResultsHere").innerText = temp 
+}
