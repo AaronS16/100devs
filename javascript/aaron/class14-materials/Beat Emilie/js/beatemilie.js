8211e4a329c56ef65cc7b443a6afd0badecac619
Aaron <soteloaaron16@gmail.com>
2022-07-17 13:10:55 -0500

    fun page

---
+++ javascript/aaron/class14-materials/Beat Emilie/js/beatemilie.js
@@ -0,0 +1,56 @@
+document.querySelector('.olivia').addEventListener('click', olivia)
+document.querySelector('.selena').addEventListener('click', selena)
+document.querySelector('.halle').addEventListener('click', halle)
+document.querySelector('.mylove').addEventListener('click', emmy)
+document.querySelector('.eww').addEventListener('click', emilie)
+
+function olivia () {
+    document.querySelector('.selenaPic').style.display = 'none'
+    document.querySelector('.hallePic').style.display = 'none'
+    document.querySelector('.emmyPic').style.display = 'none'
+    document.querySelector('.emiliePic').style.display = 'none'
+    document.querySelector('.oliviaPic').style.display = 'none'
+    document.querySelector('.goAway').style.display = 'none'
+    document.body.style.backgroundImage = "url('./img/olivia.jpeg')";
+    document.body.style.backgroundSize = "auto";
+}
+function selena () {
+    document.querySelector('.selenaPic').style.display = 'none'
+    document.querySelector('.hallePic').style.display = 'none'
+    document.querySelector('.emmyPic').style.display = 'none'
+    document.querySelector('.emiliePic').style.display = 'none'
+    document.querySelector('.oliviaPic').style.display = 'none'
+    document.querySelector('.goAway').style.display = 'none'
+    document.body.style.backgroundImage = "url('./img/selena.jpeg')";
+    document.body.style.backgroundSize = "auto";
+}
+function halle () {
+    document.querySelector('.selenaPic').style.display = 'none'
+    document.querySelector('.hallePic').style.display = 'none'
+    document.querySelector('.emmyPic').style.display = 'none'
+    document.querySelector('.emiliePic').style.display = 'none'
+    document.querySelector('.oliviaPic').style.display = 'none'
+    document.querySelector('.goAway').style.display = 'none' 
+    document.body.style.backgroundImage = "url('./img/halle.jpg')";
+    document.body.style.backgroundSize = "1200px";
+}
+function emmy () {
+    document.querySelector('.selenaPic').style.display = 'none'
+    document.querySelector('.hallePic').style.display = 'none'
+    document.querySelector('.emmyPic').style.display = 'none'
+    document.querySelector('.emiliePic').style.display = 'none'
+    document.querySelector('.oliviaPic').style.display = 'none'
+    document.querySelector('.goAway').style.display = 'none'
+    document.body.style.backgroundImage = "url('./img/emmy-rossum-hed.jpg')";
+    document.body.style.backgroundSize = "cover";
+}
+function emilie () {
+    document.querySelector('.selenaPic').style.display = 'none'
+    document.querySelector('.hallePic').style.display = 'none'
+    document.querySelector('.emmyPic').style.display = 'none'
+    document.querySelector('.emiliePic').style.display = 'none'
+    document.querySelector('.oliviaPic').style.display = 'none'
+    document.querySelector('.goAway').style.display = 'none' 
+    document.body.style.backgroundImage = "url('./img/20200528_074657.jpg')";
+    document.body.style.backgroundSize = "1200px";
+}
