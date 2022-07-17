1d38e675194d1b758ea46a7890d9c543027c9741
EmilieFlores <codingtime0000@gmail.com>
2022-07-17 13:10:35 -0500

    update

--- javascript/emilie/Side-work/2/js/main.js
+++ javascript/emilie/Side-work/2/js/main.js
@@ -16,6 +16,7 @@ function jNext(){
 	ryan.classList.add("hidden")
 	zachary.classList.add("hidden")
 	j.classList.toggle("hidden")
+	document.querySelector('body').style.backgroundColor ="rgb(214, 99, 118)"
 }
 
 function tommyNext(){
@@ -24,6 +25,7 @@ function tommyNext(){
 	ryan.classList.add("hidden")
 	zachary.classList.add("hidden")
 	tommy.classList.toggle("hidden")
+	document.querySelector('body').style.backgroundColor ="rgb(71, 4, 15)"
 }
 
 function carmyNext(){
@@ -32,6 +34,7 @@ function carmyNext(){
 	ryan.classList.add("hidden")
 	zachary.classList.add("hidden")
 	carmy.classList.toggle("hidden")
+	
 }
 function rrNext(){
 	tommy.classList.add("hidden")
@@ -39,6 +42,7 @@ function rrNext(){
 	j.classList.add("hidden")
 	zachary.classList.add("hidden")
 	ryan.classList.toggle("hidden")
+	document.querySelector('body').style.backgroundColor ="rgb(173, 78, 94)"
 }
 function zachNext(){
 	tommy.classList.add("hidden")
@@ -46,4 +50,5 @@ function zachNext(){
 	ryan.classList.add("hidden")
 	j.classList.add("hidden")
 	zachary.classList.toggle("hidden")
+	document.querySelector('body').style.backgroundColor ="rgb(133, 21, 40)"
 }
