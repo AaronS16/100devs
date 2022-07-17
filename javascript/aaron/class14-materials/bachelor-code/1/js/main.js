156d75cfd060d0556e82851470766977b19990af
Aaron <soteloaaron16@gmail.com>
2022-07-17 10:36:03 -0500

    bachelor 1

--- javascript/aaron/class14-materials/bachelor-code/1/js/main.js
+++ javascript/aaron/class14-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,16 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+// document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
 
-function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+// function hide(){
+// 	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+// 	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+// }
+
+
+
+
+document.querySelector('#finalRose').addEventListener('click', rose)
+
+function rose () {
+	document.querySelector('#claire').style.display = 'none'
+	document.querySelector('#sharleen').style.display = 'none'
 }
