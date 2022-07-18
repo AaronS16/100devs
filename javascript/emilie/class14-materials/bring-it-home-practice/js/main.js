b3b7d6e98e307bef3723efc414f47fc2213f18f7
EmilieFlores <codingtime0000@gmail.com>
2022-07-18 18:58:04 -0500

    update

--- javascript/emilie/class14-materials/bring-it-home-practice/js/main.js
+++ javascript/emilie/class14-materials/bring-it-home-practice/js/main.js
@@ -1,13 +1,24 @@
 // *Variables*
 // Create a variable and console log the value
-
+let apple = 2
+console.log(apple)
 // Create a variable, add 10 to it, and alert the value
+let openHouse = 5
+let sum = openHouse + 10
+alert(sum)
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-
+function subFourNumbers( num1, num2, num3, num4){
+    const difference=( num1-num2 - num3 - num4)
+    alert(difference)
+}
+subFourNumbers(50,30,5,1)
 // Create a function that divides one number by another and returns the remainder
-
+function dividesTwoNumbers(numb1,num2){
+    return numb1 / num2
+}
+console.log(dividesTwoNumbers (20,2))
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
