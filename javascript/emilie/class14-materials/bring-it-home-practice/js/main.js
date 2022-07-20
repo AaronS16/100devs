ff69ff584fa8e3a2f45acc855e11d0e39fd355c7
EmilieFlores <codingtime0000@gmail.com>
2022-07-20 04:11:39 -0500

    update

--- javascript/emilie/class14-materials/bring-it-home-practice/js/main.js
+++ javascript/emilie/class14-materials/bring-it-home-practice/js/main.js
@@ -24,9 +24,20 @@ console.log(dividesTwoNumbers (20,2))
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 function addingTwoNumbers(num1, num2){
     const sum = num1 + num2
-    if (){
-
+    if( sum >= 50 ){
+        alert( "Jumaji")
     }
+    else alert("yay!")
 }
 
+addingTwoNumbers (40,10)
+
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function multiplyThreeNumbers(num1,num2,num3){
+    const product = num1 * num2 * num3 
+    if(product / 3 ){
+        alert("ZEBRA")
+    }
+    else alert("oh Hey!")
+}
+multiplyThreeNumbers (5,5,5)
