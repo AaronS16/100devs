38588194aed530135ed3a9104f594e7b61405595
Aaron <soteloaaron16@gmail.com>
2022-07-20 18:19:37 -0500

    class

--- javascript/aaron/class13-materials/class-weekend-boring/js/main.js
+++ javascript/aaron/class13-materials/class-weekend-boring/js/main.js
@@ -1,19 +1,16 @@
-//Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
-
 document.querySelector('#check').addEventListener('click', dayOfWeek)
 
 function dayOfWeek () {
+    const day = document.querySelector('#day').value.toLowerCase()
 
-  const day = document.querySelector('#day').value.toLowerCase()
-
-  if (day === 'monday' || day === 'wednesday' || day === 'friday') {
-    document.querySelector('#placeToSee').innerText = "Boring day"
-  }
-  else if (day === 'tuesday' || day === 'thursday') {
-    document.querySelector('#placeToSee').innerText = 'Class Day'
-  }
-  else if (day === 'saturday' || day === 'sunday') {
-    document.querySelector('#placeToSee').innerText = "Weekend Yay!!!"
-  }
-  else alert('Please enter in a day')
+    if (day === 'monday' || day === 'wednesday' || day === 'friday') {
+        document.querySelector('#placeToSee').innerText = "Boring Day..."
+    }
+    else if (day === 'tuesday' || day === 'thursday') {
+        document.querySelector('#placeToSee').innerText = 'Class tonight!'
+    }
+    else if (day === 'saturday' || day === 'sunday') {
+        document.querySelector('#placeToSee').innerText = 'The WEEKEND!!!'
+    }
+    else alert('Please enter in a day of the week')
 }
