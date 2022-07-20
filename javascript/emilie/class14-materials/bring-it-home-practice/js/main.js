ec7249a891f86f84650f21ca5be27226b27cf15e
EmilieFlores <codingtime0000@gmail.com>
2022-07-20 03:46:41 -0500

    update

--- javascript/emilie/class14-materials/bring-it-home-practice/js/main.js
+++ javascript/emilie/class14-materials/bring-it-home-practice/js/main.js
@@ -23,4 +23,5 @@ console.log(dividesTwoNumbers (20,2))
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
+
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
