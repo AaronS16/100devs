ec94ea6ee10443378610e02f3080c8055a1ae925
EmilieFlores <codingtime0000@gmail.com>
2022-07-21 17:38:44 -0500

    update

--- javascript/emilie/class16-materials/variable-fun-time/js/main.js
+++ javascript/emilie/class16-materials/variable-fun-time/js/main.js
@@ -1,13 +1,16 @@
 //--- Easy
 //create a variable and assign it a number
-
+let apple = 20 
 //minus 10 from that number
-
+function eatapple(){
+    let banana = apple - 10
+    console.log (banana)
+}
 //print that number to the console
 
 //--- Medium
 //create a variable that holds a value from the input
-
+ 
 //add 25 to that number
 
 //alert that number
