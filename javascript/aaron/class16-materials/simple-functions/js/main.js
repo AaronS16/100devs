1b6776b6e1e459de96e79e4c80d64e23a2491397
Aaron <soteloaaron16@gmail.com>
2022-07-22 17:28:46 -0500

    update

--- javascript/aaron/class16-materials/simple-functions/js/main.js
+++ javascript/aaron/class16-materials/simple-functions/js/main.js
@@ -1,9 +1,25 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
 
+function sub2 (num1, num2) {
+    alert(num1 - num2)
+}
+sub2(16, 9)
+
 //create a function that divides three numbers and console logs the quotient
 
+function div3 (x,y,z) {
+    console.log(x / y / z)
+}
+
+div3(27,9,3)
+
 //create a function that multiplys three numbers and returns the product
+
+function mult3 (x, y,z) {
+    return x * y * z
+}
+console.log(mult3(2,8,7))
 
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
