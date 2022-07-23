44f9213927c6398d587666bf8c385e77fd73fc64
EmilieFlores <codingtime0000@gmail.com>
2022-07-23 12:15:55 -0500

    update

--- javascript/emilie/class16-materials/bring-it-on/js/main.js
+++ javascript/emilie/class16-materials/bring-it-on/js/main.js
@@ -1,11 +1,11 @@
 // *Variables*
 // Create a variable and console log the value
-let n = 10 
-console.log(n)
+let n = 10; 
+console.log(n);
 // Create a variable, add 10 to it, and alert the value
-let number = 20
-let add10 = number + 10
-alert(add10)
+let number = 20;
+let add10 = number + 10;
+alert(add10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
@@ -20,3 +20,7 @@ alert(add10)
 
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
+
+const name = prompt("Enter your name: ");
+const name2 = prompt("Enter your last name: ")
+alert(`Hello, ${name +  name2}`);
