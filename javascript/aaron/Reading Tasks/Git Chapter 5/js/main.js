fba45b5de59200288023d71ae3945b0c6a5792db
Aaron <soteloaaron16@gmail.com>
2022-07-25 16:59:58 -0500

    update

--- javascript/aaron/Reading Tasks/Git Chapter 5/js/main.js
+++ javascript/aaron/Reading Tasks/Git Chapter 5/js/main.js
@@ -13,3 +13,31 @@ alert(sayHello(firstName, lastName))
 
 //Number Squaring
 
+let x = 2
+
+function square1(x) {
+    let square = x * x
+    return square
+  }
+  
+  
+  const square2 = x => x * x
+  
+  console.log(square1(0)); 
+  console.log(square1(2)); 
+  console.log(square1(5)); 
+  
+  console.log(square2(0)); 
+  console.log(square2(2)); 
+  console.log(square2(5));
+
+  // Minimum of Two Numbers
+
+function min () {
+    
+}
+
+
+console.log(min(4.5, 5));
+console.log(min(19, 9)); 
+console.log(min(1, 1));
