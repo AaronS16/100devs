61ac39ef9e03a4626ab9ba97b0ee09df7315f868
Aaron <soteloaaron16@gmail.com>
2022-07-25 14:11:11 -0500

    update

---
+++ javascript/aaron/Reading Tasks/Git Chapter 5/js/main.js
@@ -0,0 +1,15 @@
+//Improved Hello
+
+function sayHello(firstName, lastName) {
+    const message = `Hello, ${firstName} ${lastName}!`;
+    return message;
+  }
+
+let firstName = prompt("What's your first name?")
+
+let lastName = prompt("What's your last name?")
+
+alert(sayHello(firstName, lastName))
+
+//Number Squaring
+
