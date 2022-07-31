1bf08a34275e9caf77bdcdea3e3c4d4e8f1202f1
Aaron <soteloaaron16@gmail.com>
2022-07-31 07:50:19 -0500

    fix change

--- javascript/aaron/class16-materials/stop-snacking/js/main.js
+++ javascript/aaron/class16-materials/stop-snacking/js/main.js
@@ -1,11 +1,11 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
 
-// document.querySelector('#help').addEventListener('click', stopSnack)
+document.querySelector('#help').addEventListener('click', stopSnack)
 
-// function stopSnack () {
-//     let stop = Number(document.querySelector('input').value)
-//     document.querySelector('#stops').innerText = ''
-//     for (i = 1; i <= stop; i++){
-//     document.querySelector('#stops').innerText += ' Stop snacking '
-//     }
-// }
+function stopSnack () {
+    let stop = Number(document.querySelector('input').value)
+    document.querySelector('#stops').innerText = ''
+    for (i = 1; i <= stop; i++){
+    document.querySelector('#stops').innerText += ' Stop snacking '
+    }
+}
