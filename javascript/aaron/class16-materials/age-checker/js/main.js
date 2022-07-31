3e2bd2a386d88a6340d4c72592774cd20476b9eb
Aaron <soteloaaron16@gmail.com>
2022-07-31 09:38:12 -0500

    age checker

--- javascript/aaron/class16-materials/age-checker/js/main.js
+++ javascript/aaron/class16-materials/age-checker/js/main.js
@@ -1,19 +1,6 @@
 //Create a conditonal that checks their age
 
-let input = document.querySelector('#danceDanceRevolution').addEventListener('click', checkAge)
-
-function checkAge () {
-    if (input <= '16') {
-        alert('You can not drive')
-    }
-    else if (input <= '17') {
-        alert("You can't hate from outside the club, you can't even get in")
-    }
-    else if (input <= '20') {
-        
-    }
-}
-
+// 
 //If under 16, tell them they can not drive
 
 
@@ -36,3 +23,43 @@ function checkAge () {
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+Number(document.querySelector('#num').addEventListener('click', checkAge))
+
+function checkAge () {
+    let input = document.querySelector('input').value
+    if (input < 16) {
+        alert(`Since you are ${input} you can not drive`)
+    }
+    else if (input < 18) {
+        alert(`Since you are ${input} you can't hate from outside the club, because you can't even get in`)
+    }
+    else if (input < 21) {
+        alert(`Since you are ${input} you can't drink yet`)
+    }
+    else if (input < 25) {
+        alert(`Since you are ${input} you can't rent cars affordably`)
+    }
+    else if (input < 30) {
+        alert(`Since you are ${input} you can't rent fancy cars at a good price yet`)
+    }
+    else alert(`Since you are ${input} there is nothing left for you in life`)
+}
+
+
+
+
+
+// let input = document.querySelector('#danceDanceRevolution').addEventListener('click', checkAge)
+
+// function checkAge () {
+//     if (input <= '16') {
+//         alert('You can not drive')
+//     }
+//     else if (input <= '17') {
+//         alert("You can't hate from outside the club, you can't even get in")
+//     }
+//     else if (input <= '20') {
+        
+//     }
+// }
