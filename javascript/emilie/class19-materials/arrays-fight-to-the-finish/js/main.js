e8e74f105715233d54553b1a1f1195256d647589
EmilieFlores <codingtime0000@gmail.com>
2022-08-02 18:46:20 -0500

    update

--- javascript/emilie/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ javascript/emilie/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,14 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
-
+let movies = ['bring it on 1', 'bring it on 2']
+for(i = 0; i < movies.length; i++){
+    document.querySelector('h2').innerText += movies[i]
+}
+movies()
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
+let numbers = [1,2,3,4]
+for (i = 0; i < numbers){
 
+}
 
 //Find the average of all the numbers from question three
