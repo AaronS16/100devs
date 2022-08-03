e8f0b109793c4cc5a50af0713a4a20cead241001
Aaron <soteloaaron16@gmail.com>
2022-08-03 09:15:50 -0500

    moving code wars

--- javascript/aaron/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ javascript/aaron/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -28,18 +28,3 @@ console.log(sum / numbers.length)
 
 
 
-function countBy(x, n) {
-    let z = [];
-    for (let i = 1; i <= n; i++) {
-      z.push(x * i);
-    }
-    return z;
-  }
-  console.log(countBy(2,8))
-
-  function even_or_odd(number) {
-    if (number % 2 === 0) {
-      console.log('Even')
-    } else console.log('Odd')
-  }
-  even_or_odd(2)
