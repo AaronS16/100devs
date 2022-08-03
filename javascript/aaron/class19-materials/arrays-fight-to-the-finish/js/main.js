e00c6bc6c7bfa1dbb7aa606f55874733683d6f8f
Aaron <soteloaaron16@gmail.com>
2022-08-02 19:12:39 -0500

    arrays

--- javascript/aaron/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ javascript/aaron/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,25 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
+let movies = ['Bobs Burgers Movie', 'The Package', 'Blade']
+
+for (i=0; i < movies.length; i++) {
+    document.querySelector('h2').innerText += movies[i]
+}
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 
+let numbers = [3, 6, 9]
+
+numbers.forEach((item, index) => {
+    numbers[index] = item + 3
+    console.log(numbers[index])
+})
 
 //Find the average of all the numbers from question three
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+}
+console.log(sum / numbers.length)
