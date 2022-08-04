6a2fe49d536cba69294617d3b1bbada0005eeb53
EmilieFlores <codingtime0000@gmail.com>
2022-08-04 18:07:22 -0500

    update

--- javascript/emilie/class20-materials/review-in-paradise/js/main.js
+++ javascript/emilie/class20-materials/review-in-paradise/js/main.js
@@ -1,19 +1,34 @@
 // *Variables*
 // Declare a variable, reassign it to your favorite food, and alert the value
-
+let panda = 'panda express'
+panda = 'food'
+alert(panda)
 //Declare a variable, assign it a string, alert the second character in the string (Use your google-fu and the MDN)
-
+let str = 'My dogs love me more!'
+alert (str.charAt(1))
 
 // *Functions*
 // Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
-
+function numbers (num1, num2,num3){
+    alert(num1 / num2 * num3)
+}
+numbers(5,5,5)
 
 // Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
+function cube (num1){
+console.log(Math.cbrt(num1).toFixed(5))
+}
 
-
+cube(5)
 // *Conditionals*
 //Create a function that takes in a month. If it is a summer month alert "YAY". If another other month, alert "Booo"
-
-
+function month (month){
+    if (month === 'june' || month === 'july'){
+        alert('Summer!!');
+    } else {
+        alert('BOOOO!!');
+    }
+}
+month('june')
 //*Loops*
 //Create a function that takes in a number. Console log every number from 1 to that number while skipping multiples of 5.
