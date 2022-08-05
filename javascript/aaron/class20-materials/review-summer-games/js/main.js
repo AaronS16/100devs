6d7eba71c67aeda96ce243cbd1d1feb05b1fa4cc
Aaron <soteloaaron16@gmail.com>
2022-08-05 17:59:52 -0500

    summer games

--- javascript/aaron/class20-materials/review-summer-games/js/main.js
+++ javascript/aaron/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+
+function multiply(arr) {
+    let product = 1
+    arr.forEach(num => product *= num)
+    alert(product)
+}
+
+multiply([1, 2, 3, 4, 5, 6]);
