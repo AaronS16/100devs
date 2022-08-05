6c61269d2ad9cb95caecf05ef2e74fed88c848a9
EmilieFlores <codingtime0000@gmail.com>
2022-08-05 17:57:24 -0500

    update

--- javascript/emilie/class20-materials/review-summer-games/js/main.js
+++ javascript/emilie/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,7 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function MultiplyNums(){
+    let product = 1 
+    Array.forEach( num => product *= num )
+    alert(product)
+} 
+MultiplyNums([10,])
