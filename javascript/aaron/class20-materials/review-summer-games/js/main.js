09c7928f091004fbfed3d380b0369832e22bdbfd
Aaron <soteloaaron16@gmail.com>
2022-08-06 12:26:46 -0500

    coding war

--- javascript/aaron/class20-materials/review-summer-games/js/main.js
+++ javascript/aaron/class20-materials/review-summer-games/js/main.js
@@ -10,4 +10,13 @@ function multiply(arr) {
 multiply([1, 2, 3, 4, 5, 6]);
 
 
-  
+// function positiveSum(arr) {
+//     let sum = 0;
+   
+//    for (let i=0; i < arr.length; i++) {
+//      if (arr[i] >= 0) sum += arr[i];
+//    }  
+//    return sum;
+//  }
+
+//  console.log(positiveSum([-1,2,-3,4,-5,6,-7]))
