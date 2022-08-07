8880ec73e3035af801559e2889c60a999971bd6a
EmilieFlores <codingtime0000@gmail.com>
2022-08-07 12:50:30 -0500

    update

---
+++ javascript/emilie/Readings/main.js
@@ -0,0 +1,25 @@
+// modeling a dog
+// const cali ={
+//     name: 'cali',
+//     species: 'GS',
+//     size: 'large'
+// }
+// cali.bark = 'Grrr!'
+// describe(dog){
+//     console.log(`${dog.name} is a ${dog.species} dog measuring as a ${dog.size}`);
+//     console.log(`look, a cat! ${dog.name} barks: ${dog.bark}`);
+// }
+
+//modeling a circle //
+//const r = Number(prompt('Enter the circle radius:'));//
+
+//modeling a bank account //
+const account ={
+    name: 'Alex',
+    balance: 0,
+    describe: 
+}
+
+function credit(addingNum){
+    
+}
