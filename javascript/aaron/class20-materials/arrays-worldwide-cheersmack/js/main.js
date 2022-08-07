ce1dc4a2c036b6f56ce89772855e4d36d9e0551a
Aaron <soteloaaron16@gmail.com>
2022-08-07 12:22:35 -0500

    worldwide arrays

--- javascript/aaron/class20-materials/arrays-worldwide-cheersmack/js/main.js
+++ javascript/aaron/class20-materials/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,12 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+/*Create a function that takes in a number. The function should return an array that contains every number from 1 to 
+that number as seperate elements */
+
+function toNumber (arr) {
+    let newArr = []
+    for (i = 1; i <= arr; i++) {
+        newArr.push(i)
+    }
+    return newArr
+}
+
+console.log(toNumber(15))
