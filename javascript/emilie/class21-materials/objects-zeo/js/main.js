72097fbd03b166da51f93c6cd70f5e53b8522c1a
EmilieFlores <codingtime0000@gmail.com>
2022-08-10 18:04:37 -0500

    update

--- javascript/emilie/class21-materials/objects-zeo/js/main.js
+++ javascript/emilie/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,12 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    color:'blue',
+    size:'small',
+    weight:'light',
+   
+}
+return `my watch is a ${color},${size},and has a weight that is ${weight}.`
+// stopwatch.color = 'red'
+// stopwatch.weight ='heavy'
+// stopwatch.size ='large'
+
