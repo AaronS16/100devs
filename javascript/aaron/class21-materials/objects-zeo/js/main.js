c3cb32dd2721a18b61860dc172eeb08d9aaed44a
Aaron <soteloaaron16@gmail.com>
2022-08-10 18:19:37 -0500

    objects

--- javascript/aaron/class21-materials/objects-zeo/js/main.js
+++ javascript/aaron/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,21 @@
 //Create a stopwatch object that has four properties and three methods
+
+
+const stopwatch = {}
+
+stopwatch.color = 'black'
+stopwatch.brand = 'Under Armour'
+stopwatch.shape = 'oval'
+stopwatch.price = 50
+
+stopwatch.start = function () {
+    console.log('Starting')
+}
+
+stopwatch.stop = function() {
+    console.log('Stopping')
+}
+
+stopwatch.sayBrand = function () {
+    console.log(`${this.brand}`)
+} 
