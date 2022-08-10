92413263275258d23ab676583833fa6d5af8c89d
Aaron <soteloaaron16@gmail.com>
2022-08-10 17:09:32 -0500

    updtae

--- javascript/aaron/class21-materials/alien-rangers-review/js/main.js
+++ javascript/aaron/class21-materials/alien-rangers-review/js/main.js
@@ -9,7 +9,11 @@ shows.forEach( show => console.log(show))
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
 
+let nums = [21, 56, 88, 24, 99, 710]
 
+let onlyEvens = arr => arr.filter(n => n % 2 === 0)
+
+console.log(onlyEvens(nums))
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
