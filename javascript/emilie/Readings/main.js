fda20b8108303e68972fda363286089d6e8e8c3a
EmilieFlores <codingtime0000@gmail.com>
2022-08-11 11:50:50 -0500

    update

--- javascript/emilie/Readings/main.js
+++ javascript/emilie/Readings/main.js
@@ -25,9 +25,42 @@
 // }
 
 //object reading//
-let user = {
-    name: 'John',
-    surname: 'Smith',
+// let user = {
+//     name: 'John',
+//     surname: 'Smith',
+// }
+// user.name = 'Pete';
+// delete user.name; 
+
+// function isEmpty(obj) = {
+//     for(let key in obj){
+//         return false;
+//     }
+//     return true;
+// }
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+// // let sum = salaries.John + salaries.Ann + salaries.Pete
+// // console.log(sum)
+// let sum = 0;
+// for(let key in salaries){
+//     sum += salaries[key];
+// }
+// alert(sum);
+let obj ={
+    hello: 100,
+    goodbye:300,
+    welcome: 3452,
 }
-user.name = 'Pete';
-delete user.name; 
+let product = 0;
+function multiplyNumeric(obj){
+    for(let key in obj){
+        if (typeof obj[key] == 'number'){
+            obj[key] *= 2;
+        }
+    }
+}
