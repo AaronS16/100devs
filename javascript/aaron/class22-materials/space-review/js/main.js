7bccc26b74c01758bd78afd63efcb25f557bb0f4
Aaron <soteloaaron16@gmail.com>
2022-08-11 18:26:48 -0500

    reduce method

--- javascript/aaron/class22-materials/space-review/js/main.js
+++ javascript/aaron/class22-materials/space-review/js/main.js
@@ -2,11 +2,23 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
+let nums = [1, 2, 3, 4, 5, 6, 7];
+
+let sum = nums.reduce((acc, c) => acc + c)
+
+alert(sum)
+
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+
+
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+
