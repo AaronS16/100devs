fab0b3bf146868abc0e1ab561253bb568a881c23
Aaron <soteloaaron16@gmail.com>
2022-08-11 16:16:16 -0500

    Objects

---
+++ javascript/aaron/Reading Tasks/Objects/js/main.js
@@ -0,0 +1,33 @@
+//Object Test 1
+const user = {};
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
+//Object Test 2
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
+
+//Object Test 3
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+let sum = 0;
+for(let key in salaries) {
+    sum += salaries[key];
+}
+
+alert(sum);
+
+//Object Test 4
+
