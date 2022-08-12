4d56ce03eed9e9bb23e0cbd92462113627f8d253
Aaron <soteloaaron16@gmail.com>
2022-08-12 18:13:33 -0500

    streetfighter finish

--- javascript/aaron/class22-materials/objects-street-fighter/js/main.js
+++ javascript/aaron/class22-materials/objects-street-fighter/js/main.js
@@ -1,3 +1,20 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
 
+function MakeCharacter (name, fightStyle, height, age) {
+    this.name = name;
+    this.fightStyle = fightStyle;
+    this.height = height;
+    this.age = age;
+    
+    this.victoryCheer = function() {
+        coneole.log('Whoo hoo');
+    }
+    this.losingCheer = function() {
+        console.log('Aww man');
+    }
+    this.drawCheer = function() {
+        console.log('Good fight');
+    }
+}
 
+let fighterAaron = new MakeCharacter('Aaron', 'Boxing', "5'11", 30 )
