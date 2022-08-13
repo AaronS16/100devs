fe10ef3d077c77cdb5af22c841fd9eb8fc0bbf91
Aaron <soteloaaron16@gmail.com>
2022-08-13 08:10:00 -0500

    factory

--- javascript/aaron/Reading Tasks/Object-orinted Programming/js/main.js
+++ javascript/aaron/Reading Tasks/Object-orinted Programming/js/main.js
@@ -1,13 +1,10 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1, 
-        y: 1
-    },
+function createCircle(radius) {
+    return {
+    radius,
     draw: function() {
         console.log('draw')
     }
+}
+}
 
-};
-
-circle.draw();
+const circle = createCircle(1);
