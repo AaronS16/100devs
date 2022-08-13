2a15b4aaaf65a00b4a77d743a50b8dc4421c4575
EmilieFlores <codingtime0000@gmail.com>
2022-08-13 09:11:20 -0500

    update

--- javascript/emilie/class22-materials/objects-lost-galaxy/js/main.js
+++ javascript/emilie/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {}
+
+mouse.brand = 'Logitech'
+mouse.color = 'black'
+mouse.model = 'Mx Ergo'
+mouse.wireless = true 
+
+mouse.leftClick = function (){
+    console.log('Left Clickkk')
+}
+mouse.rightClick = function (){
+    console.log('RIGHT CLICKKK')
+} 
+mouse.scroll = function (){
+    console.log('JUMP JUMP JUMP')
+}
