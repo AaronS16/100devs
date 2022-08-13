17311cffd34b9b207b9400f0216ed0e0bed7b61e
EmilieFlores <codingtime0000@gmail.com>
2022-08-13 09:55:29 -0500

    update

--- javascript/emilie/class22-materials/objects-street-fighter/js/main.js
+++ javascript/emilie/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,16 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function MakeSFCharacter(punch, kick, catchPhrase,specialMove){
+    this.punch = punch
+    this.kick = kick
+    this.catchPhrase = catchPhrase
+    this.specialMove = specialMove
+    this.taunt = function(){
+        console.log(`you can't handle my ${specialMove}`)   
+    }
+    this.winning = function(){
+        console.log(`Hahah! ${this.catchPhrase}`)
+    }
+    this.dash = function(){
+        console.log(`Whoop, missed me!`)
+    }
+}
