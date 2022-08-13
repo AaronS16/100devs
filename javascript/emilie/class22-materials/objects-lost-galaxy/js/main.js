3c996c9db8c5602f9f61cd29e9b4b3e4d75da2e5
EmilieFlores <codingtime0000@gmail.com>
2022-08-13 09:14:25 -0500

    update

--- javascript/emilie/class22-materials/objects-lost-galaxy/js/main.js
+++ javascript/emilie/class22-materials/objects-lost-galaxy/js/main.js
@@ -15,3 +15,37 @@ mouse.rightClick = function (){
 mouse.scroll = function (){
     console.log('JUMP JUMP JUMP')
 }
+
+let mouse2 = {}
+
+mouse.brand = 'Logitech'
+mouse.color = 'black'
+mouse.model = 'Pro Wireless'
+mouse.wireless = true 
+
+mouse.leftClick = function (){
+    console.log('Left Clickkk')
+}
+mouse.rightClick = function (){
+    console.log('RIGHT CLICKKK')
+} 
+mouse.scroll = function (){
+    console.log('JUMP JUMP JUMP')
+} 
+
+let mouse3 = {}
+
+mouse.brand = 'Apple'
+mouse.color = 'silver'
+mouse.model = 'Magic Trackpad 2'
+mouse.wireless = true 
+
+mouse.leftClick = function (){
+    console.log('Left Clickkk')
+}
+mouse.rightClick = function (){
+    console.log('RIGHT CLICKKK')
+} 
+mouse.scroll = function (){
+    console.log('slide two fingers')
+}
