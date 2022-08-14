b80102792d643c4494fb39cfe5d8316327d21b39
Aaron <soteloaaron16@gmail.com>
2022-08-14 09:26:10 -0500

    update

--- javascript/aaron/Reading Tasks/Code Wars/js/main.js
+++ javascript/aaron/Reading Tasks/Code Wars/js/main.js
@@ -145,7 +145,6 @@
 //   console.log(maps([2, 3, 4]))
 
 // function maps(x){
-//     //return x.map(el => el * 2);
 //     let arr = [];
 //     for(let i = 0; i < x.length; i++){
 //     arr.push(x[i] * 2);
