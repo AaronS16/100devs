8f30c23fa15a2a40855e277447434cc6443bff1a
Aaron <soteloaaron16@gmail.com>
2022-08-16 18:55:01 -0500

    space review

--- javascript/aaron/class23-materials/space-review/js/main.js
+++ javascript/aaron/class23-materials/space-review/js/main.js
@@ -2,11 +2,26 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
+let nums = [2, 8, 11, 24]
+
+alert(nums.reduce((acc, c) => acc + c, 0))
+
+
+
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+
+let newArr = nums => nums.map(num => num**2)
+
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+let reverse = str => console.log( str.split('').reverse().join('') )
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+const palindromeCheck = str => alert( str ===  str.split('').reverse().join('') )
+
+palindromeCheck('racecar')
