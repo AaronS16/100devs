14bc5f1f2a81d69d49a9fefef3550ece27f21736
Aaron <soteloaaron16@gmail.com>
2022-08-17 17:52:46 -0500

    update tony hawk skater

--- javascript/aaron/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ javascript/aaron/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,13 +1,13 @@
 //Create a Tony Hawk Pro Skater constructor that makes skating game character with 4 properties and 3 methods
 
 function Skater(name, handedness, height, weight) {
-    this.name = name;
-    this.handedness = handedness;
-    this.height = height;
-    this.weight = weight;
+    this.name = name
+    this.handedness = handedness
+    this.height = height
+    this.weight = weight
 
-    this.grind = function() {
-        console.log('grinding on the rails');
+    this.flip = function() {
+        console.log('Kickflip');
     } 
     this.taunt = function() {
         console.log('Do you even skate bro?');
@@ -16,3 +16,5 @@ function Skater(name, handedness, height, weight) {
         console.log("I'm the Michael Jordan of skating");
     }
 }
+
+let darthMaul = new Skater('Darth Maul', 'Right', '6', 200)
