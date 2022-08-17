4b1251b93dd36d607af44d8517e614ae663b3f69
Aaron <soteloaaron16@gmail.com>
2022-08-17 17:27:34 -0500

    class 23 night 2

--- javascript/aaron/class23-materials/objects-lost-galaxy/js/main.js
+++ javascript/aaron/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a dog object that has four properties and three methods
+
+
+let dog = {}
+
+dog.breed = 'husky';
+dog.color = 'sable';
+dog.eyes = 'blue';
+dog.weight = '70lbs';
+
+dog.bark = function() {
+    console.log('rawr rawr');
+}
+
+dog.jump = function() {
+    console.log('Blast off');
+}
+
+dog.fetch = function() {
+    console.log('ball time');
+}
