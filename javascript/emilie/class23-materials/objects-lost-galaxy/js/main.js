7f856b9bbcd149bfab166a9b12c7232fce19ed03
EmilieFlores <codingtime0000@gmail.com>
2022-08-18 05:43:08 -0500

    update

--- javascript/emilie/class23-materials/objects-lost-galaxy/js/main.js
+++ javascript/emilie/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,15 @@
 //Create a dog object that has four properties and three methods
+let dog = {}
+dog.breed = 'GS'
+dog.color =  'black and brown'
+dog.legs = '4'
+dog.name = 'cali'
+dog.bark= function(){
+    console.log('Woof')
+}
+dog.jump = function(){
+    console.log('BLAST OFF')
+}
+dog.fetch = function(){
+    console.log('ZOOOOMM')
+}
