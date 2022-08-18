a0c6fb7824f015a600367997ab2f15e82959e3ef
EmilieFlores <codingtime0000@gmail.com>
2022-08-18 06:13:23 -0500

    update

--- javascript/emilie/class23-materials/objects-dog-photos/js/main.js
+++ javascript/emilie/class23-materials/objects-dog-photos/js/main.js
@@ -2,7 +2,8 @@
 fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json())
     .then(data => {
-    console.log(data)
+    console.log(data.message)
+    document.querySelector('img').src =data.message
     })
     .catch(err => {
     console.log(`error ${err}`)
