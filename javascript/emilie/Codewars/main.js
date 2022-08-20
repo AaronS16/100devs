5dc50836e8cc2cc91630a50221d1746c85fdac83
EmilieFlores <codingtime0000@gmail.com>
2022-08-20 08:26:22 -0500

    update

--- javascript/emilie/Codewars/main.js
+++ javascript/emilie/Codewars/main.js
@@ -166,3 +166,5 @@ const contamination = ((text, char) => {
 
     // Given a string, you have to return a string in which each character (case-sensitive) is repeated once.//
     const doubleChar = (str) => str.split("").map(c => c + c ).join("");
+    // Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.//
+    const isDivisible = (n, x, y) => n % x == 0 && n % y == 0 ? true : false;
