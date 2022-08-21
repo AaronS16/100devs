84341246bedd08ea0498abd1c52ca993787708c6
Aaron <soteloaaron16@gmail.com>
2022-08-21 08:23:04 -0500

    reading tasks

--- javascript/aaron/Reading Tasks/Objects/js/main.js
+++ javascript/aaron/Reading Tasks/Objects/js/main.js
@@ -1,46 +1,46 @@
-//Object Test 1
-const user = {};
-user.name = 'John';
-user.surname = 'Smith';
-user.name = 'Pete';
-delete user.name;
+// //Object Test 1
+// const user = {};
+// user.name = 'John';
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// delete user.name;
 
-//Object Test 2
+// //Object Test 2
 
-function isEmpty(obj) {
-    for (let key in obj) {
-        return false;
-    }
-    return true;
-}
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//         return false;
+//     }
+//     return true;
+// }
 
-//Object Test 3
+// //Object Test 3
 
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-  }
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
 
-let sum = 0;
-for(let key in salaries) {
-    sum += salaries[key];
-}
+// let sum = 0;
+// for(let key in salaries) {
+//     sum += salaries[key];
+// }
 
-alert(sum);
+// alert(sum);
 
-//Object Test 4
+// //Object Test 4
 
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   };
 
-  function multiplyNumeric(obj) {
-    for (let key in obj) {
-      if (typeof obj[key] == 'number') {
-        obj[key] *= 2;
-      }
-    }
-  }
+//   function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//       if (typeof obj[key] == 'number') {
+//         obj[key] *= 2;
+//       }
+//     }
+//   }
