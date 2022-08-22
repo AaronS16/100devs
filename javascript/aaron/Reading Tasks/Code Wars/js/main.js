73f0bb22bf2c6f9defbc919ad459a07d49798531
Aaron <soteloaaron16@gmail.com>
2022-08-22 17:11:15 -0500

    code wars

--- javascript/aaron/Reading Tasks/Code Wars/js/main.js
+++ javascript/aaron/Reading Tasks/Code Wars/js/main.js
@@ -226,3 +226,9 @@
 //     }
 //     return total
 //   } 
+
+// Find numbers which are divisible by given number
+
+// function divisibleBy(numbers, divisor){
+//     return numbers.filter( num => num % divisor == 0);
+//    }
