6838782714377213f24fdd2fefbbc06fd4fadc51
EmilieFlores <codingtime0000@gmail.com>
2022-08-22 18:42:50 -0500

    update

--- javascript/emilie/class26-materials/objects-constructor-fun/js/main.js
+++ javascript/emilie/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+let Makepizza = {
+    constructor(pizzaSize,pizzaTopping,pizzaCrust,pizzaSauce,){
+    this.size = pizzaSize
+    this.toppings = pizzaTopping
+    this.crust = pizzaCrust
+    this.sauce = pizzaSauce
+}
+estimatedDelieveryTime (){
+    console.log('Calculating..')
+}
+burnMouth(){
+    console.log('gjsdjdkjnjkdfjk')
+}
+frisbee = (){
+    console.log('YEeeeeetttt')
+}
+}
+
+
