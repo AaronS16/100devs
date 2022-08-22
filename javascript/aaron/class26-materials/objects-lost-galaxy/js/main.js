fed8c6e56b0d7d26c9d50c61b0dff89057efeb37
Aaron <soteloaaron16@gmail.com>
2022-08-22 18:25:23 -0500

    pizza object attempt

--- javascript/aaron/class26-materials/objects-lost-galaxy/js/main.js
+++ javascript/aaron/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.cheese = 'mozzarella';
+
+pizza.topping = 'pepporoni';
+
+pizza.crust = 'stuffed';
+
+pizza.size = 'large';
+
+pizza.slice = function() {
+    console.log('Cut the pizza into 8 pieces')
+}
+
+pizza.cook = function() {
+    console.log('Cook it in the wood oven');
+}
+
+pizza.delivery = function() {
+    console.log('Deliver it to address');
+}
