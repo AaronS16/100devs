8a3d24824b19a907a002f69380d8e2e559140d46
EmilieFlores <codingtime0000@gmail.com>
2022-08-22 18:08:39 -0500

    update

--- javascript/emilie/class26-materials/objects-lost-galaxy/js/main.js
+++ javascript/emilie/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,7 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+let pizza = {}
+pizza.size = 'large'
+pizza.toppings = ['spinach', ' onion', 'jalapenos','banana peppers']
+pizza.crust = 'stuffed'
+pizza.
+pizza.
