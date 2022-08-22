04a6fa1bf869798df6e634d38de04691eaff447b
Aaron <soteloaaron16@gmail.com>
2022-08-22 17:10:41 -0500

    code wars

--- javascript/aaron/Reading Tasks/Code Wars/js/main.js
+++ javascript/aaron/Reading Tasks/Code Wars/js/main.js
@@ -217,3 +217,12 @@
 //     return answer
 //   }
   
+// Name on billboard
+
+// function billboard(name, price = 30){
+//     let total = 0
+//     for (let i = 1; i <= name.length; i++) {
+//       total += price
+//     }
+//     return total
+//   } 
