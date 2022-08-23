5d7a4db25bc7710cbe800c6196f1155a1bb8ad0e
Aaron <soteloaaron16@gmail.com>
2022-08-23 17:29:15 -0500

    constructor pizza

--- javascript/aaron/class26-materials/objects-constructor-fun/js/main.js
+++ javascript/aaron/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+
+
+function Makepizza(size, crust, cheese, topping) {
+    this.size = size
+    this.crust = crust
+    this.cheese = cheese
+    this.topping = topping
+
+    this.delivery = function() {
+        console.log('ETA...')
+    }
+    this.slice = function() {
+        console.log('Cut the pizza into 8 pieces')
+    }
+    
+    this.cook = function() {
+        console.log('Cook it in the wood oven');
+    }
+}
