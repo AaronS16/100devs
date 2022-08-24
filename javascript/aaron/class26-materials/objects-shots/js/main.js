36d71313a20a3990fbdd58ed879dfb5d4ef9d239
Aaron <soteloaaron16@gmail.com>
2022-08-24 18:53:10 -0500

    apis carousel

--- javascript/aaron/class26-materials/objects-shots/js/main.js
+++ javascript/aaron/class26-materials/objects-shots/js/main.js
@@ -10,9 +10,9 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink)
     .then(res => res.json())
     .then(data  => {
         console.log(data.drinks)
-        document.querySelector('h2').innerText = data.drinks[0 - 19].strDrink
-        document.querySelector('img').src = data.drinks[0 - 19].strDrinkThumb
-        document.querySelector('h3').innerText = data.drinks[0 - 19].strInstructions
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
     })
 }
 
