be7ae4740d8e4acf90a5f49ecb0375512bd77c5d
EmilieFlores <codingtime0000@gmail.com>
2022-08-29 17:25:56 -0500

    update

--- javascript/emilie/class27-materials/objects-netflix-tv-shows/js/main.js
+++ javascript/emilie/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,17 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
-
-
+class MakeShow{
+    constructor(showName, showLength, showMainCharacter,showTheme){
+    this.name = showName
+    this.length = showLength
+    this.main = showMainCharacter
+    this.theme = showTheme
+}
+play (){
+    alert('lets go!')
+stop(){
+    alert('Stop!!')
+}
+skip (){
+    alert('skip that shit!!')
+}
+}} 
