4e3aebcd953dc205700e3c99f7d16febed22b13a
Aaron <soteloaaron16@gmail.com>
2022-08-29 17:20:21 -0500

    show class

--- javascript/aaron/class27-materials/objects-netflix-tv-shows/js/main.js
+++ javascript/aaron/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,8 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
 
+class TvShow {
+    constructor(title, genre, rating, review) {
+
+    }
+}
