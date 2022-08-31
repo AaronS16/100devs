909ed8aba28f64e2b976eee4b4728b59b31e7636
Aaron <soteloaaron16@gmail.com>
2022-08-31 16:49:28 -0500

    add 1

--- javascript/aaron/class27-materials/local-storage-botScore-button/js/main.js
+++ javascript/aaron/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,9 +1,11 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
 
+localStorage.setItem('botScore', 0)
+
 document.querySelector('button').addEventListener('click', addOne)
 
 function addOne(){
-    localStorage.setItem('count', 1)
     
+
 }
