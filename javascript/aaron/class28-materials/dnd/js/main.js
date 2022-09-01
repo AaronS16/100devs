4f3628a21cf19af0f8f83731b39476bbcd422a87
Aaron <soteloaaron16@gmail.com>
2022-09-01 17:52:34 -0500

    API

--- javascript/aaron/class28-materials/dnd/js/main.js
+++ javascript/aaron/class28-materials/dnd/js/main.js
@@ -8,7 +8,7 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+       console.log(data.results)
       })
       .catch(err => {
           console.log(`error ${err}`)
