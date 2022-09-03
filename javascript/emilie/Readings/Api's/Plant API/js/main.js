25bc18fac0f9cca319e17da222f81563bd138e5a
EmilieFlores <codingtime0000@gmail.com>
2022-09-03 14:30:16 -0500

    update

--- javascript/emilie/Readings/Api's/Plant API/js/main.js
+++ javascript/emilie/Readings/Api's/Plant API/js/main.js
@@ -2,11 +2,16 @@ document.querySelector('button').addEventListener('click', getMario)
 
 function getMario() {
 
-  fetch('https://mario-kart-tour-api.herokuapp.com/api/v1/drivers/name?q=mario', {
-    method: "GET",
-    headers: {"Content-type": "application/json;charset=UTF-8"}
+let choice = Math.floor(Math.random() * 100) 
+
+fetch(`https://mario-kart-tour-api.herokuapp.com/api/v1/drivers/name?q=mario/${choice}`)
+  .then( res => res.json())
+  .then( data => {
+    console.log(data) 
+    console.log(choice)
+    document.querySelector('img').src = data.image 
+    document.querySelector('h2').innerText = data.name
+    
   })
-   .then(response => response.json()) 
-   .then(json => console.log(json))
-   document.querySelector('h2').innerText = data.name
+
 }
