a53a11990e6003334ff43dfb0423cf362db82e5d
Aaron <soteloaaron16@gmail.com>
2022-09-03 15:31:29 -0500

    mario api

--- javascript/aaron/Reading Tasks/Mario API/js/main.js
+++ javascript/aaron/Reading Tasks/Mario API/js/main.js
@@ -1,14 +1,15 @@
 document.querySelector('button').addEventListener('click', getDog)
 
+
 function getDog() {
 
+let choice = document.querySelector('input').value
 
-
-fetch('https://www.amiiboapi.com/api/amiibo/?name=zelda')
+fetch(`https://www.amiiboapi.com/api/amiibo/?name=${choice}`)
   .then( res => res.json())
   .then( data => {
     console.log(data.amiibo[0]) 
-
+    document.querySelector('h2').innerText = data.amiibo[0].name
     document.querySelector('img').src = data.amiibo[0].image
 
   })
