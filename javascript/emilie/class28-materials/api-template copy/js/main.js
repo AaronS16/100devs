330216a46a1a4cf68f636329f0e6a8528b665faf
EmilieFlores <codingtime0000@gmail.com>
2022-09-06 18:21:23 -0500

    update

--- javascript/emilie/class28-materials/api-template copy/js/main.js
+++ javascript/emilie/class28-materials/api-template copy/js/main.js
@@ -1,6 +1,6 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
-
+document.querySelector('h2').innerText = localStorage.getItem('books')
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
@@ -10,7 +10,9 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('h2').innerText = data.title
+        // put title into localstorage 
+        localStorage.setItem('books',data.title)
+        document.querySelector('h2').innerText = localStorage.getItem('books')
       })
       .catch(err => {
           console.log(`error ${err}`)
