0de85c94c6a0bccf8e159081398b196bbcce4348
Aaron <soteloaaron16@gmail.com>
2022-09-12 18:44:29 -0500

    reduce method

--- javascript/aaron/class29-materials/cascade-badge/js/main.js
+++ javascript/aaron/class29-materials/cascade-badge/js/main.js
@@ -1,11 +1,19 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
 
+function reverseList(party) {
+    console.log( party.reverse())
+}
 
+reverseList(['charmander', 'pikachu', 'bulbasaur'])
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
+function checkAGreaterThanB(a, b) {
+    return a.reduce( (prev, cur) => prev + cur**2, 0) > b.reduce( (prev, cur) => prev + cur**3, 0)
+}
 
+console.log(checkAGreaterThanB([2,2,2][2,2,2]))
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
