f2e97d819e9636512688e8ec9b325769ac1fc76c
Aaron <soteloaaron16@gmail.com>
2022-09-14 17:47:13 -0500

    class

--- javascript/aaron/class30-materials/objects-espresso-machine/js/main.js
+++ javascript/aaron/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+
+class EspressoMachine {
+    constructor(manufacturer, brand, model, color) {
+        this.manufacturer = manufacturer
+        this.brand = brand
+        this.model = model
+        this.color = color
+    }
+    brewCoffee() {
+        console.log('Make a cup of coffee')
+    }
+    boilWater(){
+        console.log('Boil the water')
+    }
+    powerOn(){
+        console.log('Turn on machine')
+    }
+}
