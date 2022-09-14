3ef6b8e1440f4a3ccbea447cfbc243efd951d5a6
EmilieFlores <codingtime0000@gmail.com>
2022-09-14 17:45:06 -0500

    update

--- javascript/emilie/class30-materials/objects-espresso-machine/js/main.js
+++ javascript/emilie/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine{
+    constructor(color,make,model,price){
+        this.color = color
+        this.make = make
+        this.model = model
+        this.price = price 
+    }
+    turnOn(){
+        console.log('Good day, I am now on!')
+    }
+    steam(){
+        console.log('ITTSSS SUMMER SALLEEE 50%%')
+    }
+    brew(){
+        console.log('Good STUFF coming your way')
+    }
+}
+
+let gaggia = new EspressoMachine('red','Gaggia','classic Pro',)
