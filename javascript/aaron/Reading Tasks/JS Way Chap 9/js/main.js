c48ffd6afa51bf7266dcc705f986e16200585b35
Aaron <soteloaaron16@gmail.com>
2022-09-17 13:20:20 -0500

    Character RPG

--- javascript/aaron/Reading Tasks/JS Way Chap 9/js/main.js
+++ javascript/aaron/Reading Tasks/JS Way Chap 9/js/main.js
@@ -49,10 +49,14 @@ class Character {
         } else {
           target.health = 0;
           const bonusXP = 10;
+          const bonusGold = 10
+          const bonusKey = 1
           console.log(
-            `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`
+            `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${bonusGold} gold and ${bonusKey} key(s)`
           );
           this.xp += bonusXP;
+          this.gold += bonusGold
+          this.key += bonusKey;
         }
       } else {
         console.log(`${this.name} can't attack (they've been eliminated)`);
@@ -61,6 +65,24 @@ class Character {
     // Return the character description
     describe() {
       return `${this.name} has ${this.health} health points, ${this
-        .strength} as strength and ${this.xp} XP points`;
+        .strength} as strength, ${this.xp} XP points, ${this.gold} gold and ${this.key} key(s)`;
     }
   }
+
+  const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
+
+console.log(aurora.describe());
+console.log(glacius.describe());
