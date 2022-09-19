8e41ec55d8df14c9ee7ca21852ee8c9d9d570fbf
Aaron <soteloaaron16@gmail.com>
2022-09-19 16:36:52 -0500

    extending classes

--- javascript/aaron/class31-materials/objects-child-class/js/main.js
+++ javascript/aaron/class31-materials/objects-child-class/js/main.js
@@ -4,9 +4,6 @@ class Animal{
     constructor(name){
         this._name = name
     }
-    get name(){
-        return this._name
-    }
     speak(){
         console.log(`${this._name} makes a sound`)
     }
