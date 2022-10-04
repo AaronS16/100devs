e32d8eb0822b1ef7be81cd88bca07eb67a459f0e
Aaron <soteloaaron16@gmail.com>
2022-10-04 11:48:03 -0500

    review

--- javascript/aaron/super-review/sir-savage-the-21st/js/main.js
+++ javascript/aaron/super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,14 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+
+let sir21 = ''
+
+function savage() {
+    for( let i = 1; i <= 21; i++){
+        sir21 += ' 21'
+    }
+    document.querySelector('h2').innerText = sir21
+}
+
+savage(100)
+
 //Bonus can you make it print '21' 21 times to the dom?
