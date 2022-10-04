af315e898ef0dce2b196fae4424f6a7dca2b9e8f
Aaron <soteloaaron16@gmail.com>
2022-10-04 10:18:22 -0500

    review

--- javascript/aaron/super-review/variable-fun-time/js/main.js
+++ javascript/aaron/super-review/variable-fun-time/js/main.js
@@ -14,19 +14,34 @@ console.log(num)
 //--- Medium
 //create a variable that holds a value from the input
 
-let numb = Number(document.querySelector('#danceDanceRevolution').value)
+// let numb = Number(document.querySelector('#danceDanceRevolution').value)
 
 //add 25 to that number
 
-numb += 25
+// numb += 25
 
 //alert that number
 
-alert(numb)
+// alert(numb)
+
+document.querySelector('button').addEventListener('click', addNum)
+
+let value = 0
+
+function addNum() {
+    value = Number(document.querySelector('#danceDanceRevolution').value)
+    value += 25
+    alert(value)
+}
 
 //--- Hard
 //create a variable that holds the h1
 
 
-
 //add an event listener to that element that console logs the sum of the two previous variables
+
+document.querySelector('button').addEventListener('click', h1Value)
+
+function h1Value() {
+    answer = document.querySelector('h1').innerText = `${num + value}`
+}
