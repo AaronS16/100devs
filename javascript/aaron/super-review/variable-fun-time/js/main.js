9327a4f6c4a07138b72b91912ac8ef78f4a6cbb2
Aaron <soteloaaron16@gmail.com>
2022-10-04 09:56:34 -0500

    review

--- javascript/aaron/super-review/variable-fun-time/js/main.js
+++ javascript/aaron/super-review/variable-fun-time/js/main.js
@@ -1,9 +1,15 @@
 //--- Easy
 //create a variable and assign it a number
 
+const num = 4;
+
 //minus 10 from that number
 
+num -= 10
+
 //print that number to the console
+
+console.log(num)
 
 //--- Medium
 //create a variable that holds a value from the input
