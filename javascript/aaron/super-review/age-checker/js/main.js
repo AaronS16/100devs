4e73025134c85dd44c18495d4a10417de6182b71
Aaron <soteloaaron16@gmail.com>
2022-10-04 10:45:09 -0500

    review

--- javascript/aaron/super-review/age-checker/js/main.js
+++ javascript/aaron/super-review/age-checker/js/main.js
@@ -7,7 +7,32 @@
 //If under over 30, tell them there is nothing left to look forward too
 
 
+
+
+
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+
+document.querySelector('h1').addEventListener('click', ageChecker)
+
+function ageChecker() {
+
+    let age = Number(document.querySelector('#danceDanceRevolution').value)
+
+    if(age < 16){
+        document.querySelector('h1').innerText = 'You can not drive'
+    } else if (age < 18) {
+        document.querySelector('h1').innerText = "You can't hate from outside the club, because they can't even get in"
+    } else if( age < 21){
+        document.querySelector('h1').innerText = 'You can not drink'
+    } else if( age < 25){
+        document.querySelector('h1').innerText = 'You can not rent cars affordably'
+    }else if ( age < 30) {
+        document.querySelector('h1').innerText = 'You can not rent fancy cars affordably'
+    }else {
+        document.querySelector('h1').innerText = 'There is nothing left to look forward too'
+    }
+}
