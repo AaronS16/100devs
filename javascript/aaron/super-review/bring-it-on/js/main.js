8863e1f0bb34756ca3283c6e35fc79121b1a159c
Aaron <soteloaaron16@gmail.com>
2022-10-06 18:36:44 -0500

    review

--- javascript/aaron/super-review/bring-it-on/js/main.js
+++ javascript/aaron/super-review/bring-it-on/js/main.js
@@ -5,14 +5,26 @@ const dog = 'Oakley';
 
 // Create a variable, add 10 to it, and alert the value
 
-const num = 1;
+let num = 1;
 
 num += 10;
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
 
+function subtract4Nums(num1, num2, num3, num4){
+    alert(num1 - num2 - num3 - num4)
+}
+
+subtract4Nums(18, 6, 2, 3)
+
 // Create a function that divides one number by another and returns the remainder
+
+function div4Nums(num1, num2){
+    return num1 / num2
+}
+
+div4Nums(12, 5)
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
