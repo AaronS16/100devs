c033791353b603966efc21c3fb0b9e6293136c0a
Aaron <soteloaaron16@gmail.com>
2022-10-06 18:39:20 -0500

    review

--- javascript/aaron/super-review/bring-it-on/js/main.js
+++ javascript/aaron/super-review/bring-it-on/js/main.js
@@ -41,5 +41,13 @@ add2Nums(30, 25)
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
+function divisibleBy3(num1, num2, num3) {
+    if(num1 * num2 * num3 % 3 === 0){
+        alert('Zebra')
+    } else {
+        alert('Not divisible by 3')
+    }
+}
+
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
