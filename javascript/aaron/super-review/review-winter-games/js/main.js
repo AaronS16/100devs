c59b6e679ad45d003fd88cc308b9c504c48461ac
Aaron <soteloaaron16@gmail.com>
2022-10-07 18:38:29 -0500

    review

--- javascript/aaron/super-review/review-winter-games/js/main.js
+++ javascript/aaron/super-review/review-winter-games/js/main.js
@@ -1,2 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
 
+function array(x){
+    let newArr = []
+    for( let i = 1; i <= x.length; i++){
+        if(i % 2 == 0){
+            newArr.push(i)
+        }
+    }
+    return newArr
+}
+
+console.log(array([1,2,3,4,5,6]))
