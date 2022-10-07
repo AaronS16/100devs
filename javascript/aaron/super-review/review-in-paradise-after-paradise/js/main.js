8eba2b48239e9e9e77211bc15159b628b84a39e5
Aaron <soteloaaron16@gmail.com>
2022-10-07 18:05:02 -0500

    review

--- javascript/aaron/super-review/review-in-paradise-after-paradise/js/main.js
+++ javascript/aaron/super-review/review-in-paradise-after-paradise/js/main.js
@@ -1,3 +1,14 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
 
 
+function arr(x){
+    if(x[0] < x[x.length - 1]){
+        alert('Hi')
+    } else if((x[0] > x[x.length - 1])){
+        alert('Bye')
+    } else {
+        alert('We close in an hour')
+    }
+}
+
+arr([4, 21, 3, 4])
