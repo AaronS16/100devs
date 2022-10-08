009cc2b4ad2d370b20099e070038e2a74f2c44c4
Aaron <soteloaaron16@gmail.com>
2022-10-08 07:46:22 -0500

    review

--- javascript/aaron/super-review/objects-street-fighter/js/main.js
+++ javascript/aaron/super-review/objects-street-fighter/js/main.js
@@ -1,3 +1,23 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
 
 
+function Fighter(name, fightStyle, health, damagePerAttack){
+    this.name = name,
+    this.fightStyle = fightStyle,
+    this.health = health,
+    this.damagePerAttack = damagePerAttack
+
+    this.specialAttack = function(){
+        console.log('Super punch')
+    }
+
+    this.victoryCheer = function() {
+        console.log('I\'m the best')
+    }
+
+    this.loser = function(){
+        console.log('I need to train harder')
+    }
+}
+
+let emilie = new Fighter('Emilie', 'Kung Fu', '50', '2')
