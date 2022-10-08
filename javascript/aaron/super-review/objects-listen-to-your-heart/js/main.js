1ea1e632f70a1ae685718e07da3cc5661f1f5555
Aaron <soteloaaron16@gmail.com>
2022-10-08 07:35:57 -0500

    review

--- javascript/aaron/super-review/objects-listen-to-your-heart/js/main.js
+++ javascript/aaron/super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+
+
+let stopwatch = {}
+    stopwatch.color = 'black',
+    stopwatch.brand ='visio',
+    stopwatch.model = 'gt-50',
+    stopwatch.age = '2years'
+
+    stopwatch.stopTime = function(){
+    console.log("time stopped")
+}
+    stopwatch.startTime = function(){
+        console.log('Timer has started')
+    }
+    stopwatch.alert = function(){
+        console.log('Alarm set for 5 minutes')
+    }
