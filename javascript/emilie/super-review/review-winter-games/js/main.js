0b69d01eec17565a2312ebe2645d47042b21bea3
EmilieFlores <codingtime0000@gmail.com>
2022-10-10 12:00:54 -0500

    update

--- javascript/emilie/super-review/review-winter-games/js/main.js
+++ javascript/emilie/super-review/review-winter-games/js/main.js
@@ -1,1 +1,11 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function takesInArray(arr){
+    let only = []
+    arr.forEach(e => {
+        if(e % 2 === 0){
+            only.push(e)
+        }
+    })
+    return only 
+}
+takesInArray([1,2,3,4,5,6])
