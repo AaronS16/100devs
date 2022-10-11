3487a92d43b20c33e7ff8e2cf1e906aaf86836c6
Aaron <soteloaaron16@gmail.com>
2022-10-11 18:07:29 -0500

    review

--- javascript/aaron/class36-materials/node-backend-simple-json/server.js
+++ javascript/aaron/class36-materials/node-backend-simple-json/server.js
@@ -19,42 +19,20 @@ const server = http.createServer((req, res) => {
   console.log(page);
 
   switch (page) {
-    
+
     case '/':
       readWrite('index.html', 'text/html')
       break;
     
+    case '/otherpage':
+      readWrite('otherpage.html', 'text/html')
+      break;
     
-  }
-  if (page == '/') {
+    case '/otherotherpage':
+      readWrite('otherotherpage.html', 'text/html')
+      break;
 
-    readWrite('index.html', 'text/html')
-    // fs.readFile('index.html', function(err, data) {
-    //   res.writeHead(200, {'Content-Type': 'text/html'});
-    //   res.write(data);
-    //   res.end();
-    // });
-  }
-  else if (page == '/otherpage') {
-
-    readWrite('otherpage.html', 'text/html')
-    // fs.readFile('otherpage.html', function(err, data) {
-    //   res.writeHead(200, {'Content-Type': 'text/html'});
-    //   res.write(data);
-    //   res.end();
-    // });
-  } 
-  else if (page == '/otherotherpage') {
-
-    readWrite('otherotherpage.html', 'text/html')
-    // fs.readFile('otherotherpage.html', function(err, data) {
-    //   res.writeHead(200, {'Content-Type': 'text/html'});
-    //   res.write(data);
-    //   res.end();
-    // });
-  }
-  else if (page == '/api') {
-    // if('student' in params){
+    case '/api' :
       let personName = 'unknown',
         personOccupation = 'unknown',
         personStatus = 'unknown'
@@ -70,31 +48,28 @@ const server = http.createServer((req, res) => {
           currentOccupation: personOccupation
         }
         res.end(JSON.stringify(objToJson));
-      // }//student = leon
-      // else if(params['student'] != 'leon'){
-      //   res.writeHead(200, {'Content-Type': 'application/json'});
-      //   const objToJson = {
-      //     name: "unknown",
-      //     status: "unknown",
-      //     currentOccupation: "unknown"
-      //   }
-      //   res.end(JSON.stringify(objToJson));
-    }//student if
-     //else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
+        break;
+
+      case '/css/style.css' :
+        fs.readFile('css/style.css', function(err, data) {
+          res.write(data);
+          res.end();
+        });
+        break;
+
+      case '/js/main.js' :
+        readWrite('js/main.js', 'text/javascript')
+        break;
+
+      
+  }
+  {
+    
+  else if (page == '/js/main.js'){
 
     readWrite('js/main.js', 'text/javascript')
 
-    // fs.readFile('js/main.js', function(err, data) {
-    //   res.writeHead(200, {'Content-Type': 'text/javascript'});
-    //   res.write(data);
-    //   res.end();
-    // });
+    
   }else{
     figlet('404!!', function(err, data) {
       if (err) {
