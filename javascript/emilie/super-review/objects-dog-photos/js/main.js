6006ec933de00f8ca9ca68de850fc2ad2524adb4
EmilieFlores <codingtime0000@gmail.com>
2022-10-12 18:56:11 -0500

    updated

--- javascript/emilie/super-review/objects-dog-photos/js/main.js
+++ javascript/emilie/super-review/objects-dog-photos/js/main.js
@@ -1,8 +1,9 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-fetch()
-.then(res =>res.JSON())
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(res => res.json())
 .then(data => {
-    console.log(data)
+    console.log(data.message)
+    document.querySelector('img').src = data.message
 })
 .catch(err => {
     console.log(`error${err}`)
