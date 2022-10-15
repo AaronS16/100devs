e558c8b78fbe57b3a3cf1ecedef7573a8581a446
Aaron <soteloaaron16@gmail.com>
2022-10-15 07:01:27 -0500

    codewars

--- javascript/aaron/class36-materials/api-template/js/main.js
+++ javascript/aaron/class36-materials/api-template/js/main.js
@@ -3,11 +3,16 @@
 
 function inAscOrder(arr) {
   
-    let inOrder = arr.sort((a,b) => a - b)
-    if(inOrder === arr) {
+    let inOrder = arr.toString().split(' ').sort((a,b) => a - b).join('')
+    console.log(inOrder)
+    console.log(arr)
+    if(inOrder == arr) {
       return true
     } else {
       return false
     }
     
   }
+
+
+  console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20]))
