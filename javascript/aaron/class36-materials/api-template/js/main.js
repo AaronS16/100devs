5364c10ceecb97de05951ed1b4007774fa65726a
Aaron <soteloaaron16@gmail.com>
2022-10-15 06:54:48 -0500

    node

--- javascript/aaron/class36-materials/api-template/js/main.js
+++ javascript/aaron/class36-materials/api-template/js/main.js
@@ -1,1 +1,13 @@
 //Make an api request using async await
+
+
+function inAscOrder(arr) {
+  
+    let inOrder = arr.sort((a,b) => a - b)
+    if(inOrder === arr) {
+      return true
+    } else {
+      return false
+    }
+    
+  }
