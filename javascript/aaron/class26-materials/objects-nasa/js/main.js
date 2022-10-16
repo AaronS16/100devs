9c52e112efe0958436d222b44b32004f095b5391
Aaron <soteloaaron16@gmail.com>
2022-10-16 06:39:55 -0500

    node

--- javascript/aaron/class26-materials/objects-nasa/js/main.js
+++ javascript/aaron/class26-materials/objects-nasa/js/main.js
@@ -1,15 +1,1 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-
-
-function strCount(str, letter){  
-    let answer = 0
-    for(let i = 0; i <= str.lengeth - 1; i++){
-      if(str[i] == letter){
-        
-        answer += 1
-      }
-    }
-    return answer
-  }
-
-  console.log(strCount('Hello', 'o'))
