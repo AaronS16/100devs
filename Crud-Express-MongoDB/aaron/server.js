8f7546ab75fbb49069ea7db1ff056c2ae6034f01
Aaron <soteloaaron16@gmail.com>
2022-10-16 07:30:47 -0500

    node

--- Crud-Express-MongoDB/aaron/server.js
+++ Crud-Express-MongoDB/aaron/server.js
@@ -9,28 +9,28 @@ app.listen(3000, function() {
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const connectionString = `mongodb+srv://as:Br0C0d380@atlascluster.5jxxjqi.mongodb.net/?retryWrites=true&w=majority;`
+const connectionString = `mongodb+srv://as:Br0C0d380@atlascluster.5jxxjqi.mongodb.net/?retryWrites=true&w=majority;`;
 
-MongoClient.connect(connectionString, { useUnifiedTopology:
-    true })
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
+        console.log('Connected to Database')
         const db = client.db('star-wars-quotes')
         const quotesCollection = db.collection('quotes')
+
+
+        app.get('/', (req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    console.log(results)
+                })
+        })
+
+
         app.post('/quotes', (req, res) => {
-            quotesCollection.collection.insertOne(req.body)
-            .then(result => {
-                console.log(result)
-            })
-        }
-
-    )
-    .catch(error => console.log(error))
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+        .catch(error => console.error(error))
+    })
 })
