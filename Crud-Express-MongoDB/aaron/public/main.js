100c7dd23405ee3bd3e0e153de603a565013b76f
Aaron <soteloaaron16@gmail.com>
2022-10-16 08:08:52 -0500

    node

--- Crud-Express-MongoDB/aaron/public/main.js
+++ Crud-Express-MongoDB/aaron/public/main.js
@@ -1,5 +1,8 @@
 const update = document.querySelector('#update-button')
 
 update.addEventListener('click', _ => {
-    
+    fetch('/quotes', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'}
+    })
 })
