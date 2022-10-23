963b85d4499bcdc36ebc1171e2066aa65f50ddf7
Aaron <soteloaaron16@gmail.com>
2022-10-23 07:25:13 -0500

    codewars

--- javascript/aaron/class22-materials/objects-lost-galaxy/js/main.js
+++ javascript/aaron/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,19 +1,20 @@
 //Create a mouse object that has four properties and three methods
 
 
-const mouse = {}
+// const mouse = {}
 
-mouse.color = 'black';
-mouse.brand = 'logitech';
-mouse.shape = 'oval';
-mouse.wired = true;
+// mouse.color = 'black';
+// mouse.brand = 'logitech';
+// mouse.shape = 'oval';
+// mouse.wired = true;
 
-mouse.leftClick = function() {
-    alert('Left click');
-}
-mouse.scroll = function() {
-    console.log('Scroll down screen');
-}
-mouse.rightClick = function() {
-    alert('Right Clack');
-}
+// mouse.leftClick = function() {
+//     alert('Left click');
+// }
+// mouse.scroll = function() {
+//     console.log('Scroll down screen');
+// }
+// mouse.rightClick = function() {
+//     alert('Right Clack');
+// }
+
