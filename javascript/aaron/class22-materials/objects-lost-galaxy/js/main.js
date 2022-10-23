281f55b08496ae7e95e3b93af5fddd2350e79e06
Aaron <soteloaaron16@gmail.com>
2022-10-23 07:37:37 -0500

    codewars

--- javascript/aaron/class22-materials/objects-lost-galaxy/js/main.js
+++ javascript/aaron/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,29 +1,23 @@
 //Create a mouse object that has four properties and three methods
 
 
-// const mouse = {}
+const mouse = {}
 
-// mouse.color = 'black';
-// mouse.brand = 'logitech';
-// mouse.shape = 'oval';
-// mouse.wired = true;
+mouse.color = 'black';
+mouse.brand = 'logitech';
+mouse.shape = 'oval';
+mouse.wired = true;
 
-// mouse.leftClick = function() {
-//     alert('Left click');
-// }
-// mouse.scroll = function() {
-//     console.log('Scroll down screen');
-// }
-// mouse.rightClick = function() {
-//     alert('Right Clack');
-// }
+mouse.leftClick = function() {
+    alert('Left click');
+}
+mouse.scroll = function() {
+    console.log('Scroll down screen');
+}
+mouse.rightClick = function() {
+    alert('Right Clack');
+}
 
 
 
-function sumOfDifferences(arr) {
-    arr.sort((a,b) => a > b ? -1 : 1)
-    return arr.length > 0 ? (arr[0] - arr[1]) + (arr[1] - arr[arr.length - 1]) : 0
-      
-  }
 
-  console.log(sumOfDifferences([1]))
