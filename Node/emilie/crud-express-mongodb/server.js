bc10c688f3fe58e3e9d07e1ab3995ac7ea30dbe2
EmilieFlores <codingtime0000@gmail.com>
2022-10-30 11:36:09 -0500

    update

--- Node/emilie/crud-express-mongodb/server.js
+++ Node/emilie/crud-express-mongodb/server.js
@@ -1,1 +1,16 @@
 console.log('May Node be with you')
+const bodyParser= require('body-parser')
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true }))
+
+app.listen(3000,function(){
+    console.log('listening on 3000')
+})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+  })
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
