9df3691402c17b1828b9becfb631b205181e373a
EmilieFlores <codingtime0000@gmail.com>
2022-11-05 14:03:06 -0500

    update

--- Node/emilie/crud-express-mongodb/server.js
+++ Node/emilie/crud-express-mongodb/server.js
@@ -9,21 +9,18 @@ const connectionString = 'mongodb+srv://codetime098:kdl00LSgVKcGuCMK@cluster0.qt
 MongoClient.connect(connectionString, (err, client) => {
     if(err) return console.error(err)
     console.log('Connected to Database')
+    const db = client.db('start-wards-quotes')
+    app.use(bodyParser.urlencoded({extended: true }))
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html') 
+    })
+    app.post('/quotes', (req, res) => {
+        console.log(req.body) 
+    })
+    app.listen(3000,function(){
+        console.log('listening on 3000') 
+    })
+
+
 })
-
-app.use(bodyParser.urlencoded({extended: true }))
-
-app.listen(3000,function(){
-    console.log('listening on 3000')
-})
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
-
-
-
-
-llkkkk
+.catch(error => console.error(error))
