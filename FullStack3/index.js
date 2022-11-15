c984aea47bebeb374af8af93c0480edd2d782697
EmilieFlores <codingtime0000@gmail.com>
2022-11-14 19:12:38 -0600

    update

--- FullStack3/index.js
+++ FullStack3/index.js
@@ -1,8 +1,8 @@
 const http = require('http')
 
-const app = http.createServer((request, respone) => {
-    Response.writeHead(200, { 'Content-Type': 'text/plain'})
-    Response.end('Hello World')
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain'})
+    response.end('Hello World')
 })
 
 const PORT = 3001
