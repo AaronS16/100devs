cf5cdc0624294cc7152d7de21bef16853ed6c4ba
EmilieFlores <codingtime0000@gmail.com>
2022-11-15 19:05:36 -0600

    update

--- FullStack3/index.js
+++ FullStack3/index.js
@@ -12,7 +12,13 @@ let notes = [
         content: "Browser can execute only Javascript",
         date: "2022-05-30T18:39:34.091Z",
         important: false
-    }
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2022-05-30T19:20:14.298Z",
+        important: true
+      }
 ]
 const app = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/plain'})
