bb7f799043f22d748d826b315c5938ee2d665498
EmilieFlores <codingtime0000@gmail.com>
2022-11-26 11:21:45 -0600

    update

---
+++ To-Do-App/models/todotask.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const todoTaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    content: {
+        type: String,
+        require: true 
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
