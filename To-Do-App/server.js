ee59fb8c36b560796733be6a00c390d42a378b2b
EmilieFlores <codingtime0000@gmail.com>
2022-11-26 10:12:11 -0600

    update

--- To-Do-App/server.js
+++ To-Do-App/server.js
@@ -1,4 +1,22 @@
+//npm install express mongoose ejs dotnev 
+//nmp install --sav-dev nodemon
+
+//"start": "nodemon server.js"
+
+//Declare variables
 const express = require("express")
 const app = express()
 const PORT = 8500;
 const mongoose = require("mongoose") 
+require('dotenv').config()
+// Add model variable 
+
+// Set middleware 
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect()
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
