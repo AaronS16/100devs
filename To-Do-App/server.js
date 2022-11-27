4b138122aba732180da3b523ff3bdb621dda48d5
EmilieFlores <codingtime0000@gmail.com>
2022-11-27 12:46:29 -0600

    update

--- To-Do-App/server.js
+++ To-Do-App/server.js
@@ -4,12 +4,14 @@
 //"start": "nodemon server.js"
 
 //Declare variables
+const { response } = require("express");
 const express = require("express")
 const app = express()
 const PORT = 8500;
-const mongoose = require("mongoose") 
+const mongoose = require("mongoose"); 
+const todotask = require("./models/todotask");
 require('dotenv').config()
-// Add model variable 
+const TodoTask = require('./models/TodoTask')
 
 // Set middleware 
 app.set("view engine", "ejs")
@@ -20,6 +22,16 @@ mongoose.connect(process.env.DB_CONNECTION,
     {useNewUrlParser: true},
     () => console.log('Connected to bd!')
 )
+
+app.get('/', async (require,response) => {
+    try{
+        TodoTask.find({}, (err, tasks) => {
+         response.render('index.ejs', {todoTasks: tasks})   
+        })
+    } catch (err){
+
+    }
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
