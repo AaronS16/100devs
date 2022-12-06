477a821bcc743122854b9c32896a3a9e85ea6d28
EmilieFlores <codingtime0000@gmail.com>
2022-12-06 16:52:36 -0600

    update

--- To-Do-App/server.js
+++ To-Do-App/server.js
@@ -71,7 +71,18 @@ app
             });
         
     }); 
-    
+
+//DELETE 
+app 
+    .route("/remove/:id")
+    .get((req,res) => {
+        const id = req.params.id
+        TodoTask.findByIdAndRemove(id,err => {
+            if(err) return res.status(500).send(err)
+            res.redirect('/')
+        })
+    })
+
     
 //start server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
