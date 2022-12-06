ac92d8325ad88180990fa9e8e949aa5ab4efac3b
EmilieFlores <codingtime0000@gmail.com>
2022-12-06 14:29:11 -0600

    update

--- To-Do-App/server.js
+++ To-Do-App/server.js
@@ -49,7 +49,30 @@ app.post('/', async(req,res) => {
     }
 })
 
-
+//EDIT or UPDATE METHOD 
+app
+    .route("/edit/:id") 
+    .get((req,res) => {
+        const id = req.params.id
+        TodoTask.find({}, (err,tasks) => {
+            res.render('edit.ejs', {
+                todoTasks:tasks, idTask: id })
+            })
+        })
+    .post((req,res) => {
+        const id = req.params.id
+        TodoTask.findByIdAndUpdate(
+            id,
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+            err => {
+                if(err) return res.status(500).send(err)
+                res.redirect('/')
+            }
+        )
+    })    
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
