44d640d34b81f78111b67870b35dc9ea186c7545
EmilieFlores <codingtime0000@gmail.com>
2022-12-06 16:40:36 -0600

    update

--- To-Do-App/server.js
+++ To-Do-App/server.js
@@ -4,18 +4,17 @@
 //"start": "nodemon server.js"
 
 //Declare variables
-const { response } = require("express");
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const PORT = 8500;
 const mongoose = require("mongoose"); 
-const TodoTask = require('./models/todoTask');
+const TodoTask = require('./models/TodoTask');
 require('dotenv').config()
 
 // Set middleware 
 app.set("view engine", "ejs")
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect(process.env.DB_CONNECTION,
     {useNewUrlParser: true},
@@ -32,47 +31,48 @@ app.get("/", async (req, res) => {
     }
 });
 //Post METHOD
-app.post('/', async(req,res) => {
+app.post('/', async (req,res) => {
     const todoTask = new TodoTask(
         {
             title: req.body.title,
             content: req.body.content
-        }
-    )
+        });
     try {
         await todoTask.save()
         console.log(todoTask)
-        res.redirect("/")
+        res.redirect("/");
     } catch(err){
-        if(err) return res.status(500).send(err)
-        res.redirect('/')
+        if(err) return res.status(500).send(err);
+        res.redirect('/');
     }
-})
+});
 
 //EDIT or UPDATE METHOD 
 app
     .route("/edit/:id") 
     .get((req,res) => {
-        const id = req.params.id
+        const id = req.params.id;
         TodoTask.find({}, (err,tasks) => {
-            res.render('edit.ejs', {
-                todoTasks:tasks, idTask: id })
-            })
+            res.render("edit.ejs", { todoTasks:tasks, idTask: id });
+            });
         })
     .post((req,res) => {
-        const id = req.params.id
-        TodoTask.findByIdAndUpdate(
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate( 
             id,
             {
                 title: req.body.title,
                 content: req.body.content
             },
-            err => {
-                if(err) return res.status(500).send(err)
-                res.redirect('/')
-            }
-        )
-    })    
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+            err => {
+                if(err) return res.status(500).send(err);
+                res.redirect('/');
+            });
+        
+    }); 
+    
+    
+//start server
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
